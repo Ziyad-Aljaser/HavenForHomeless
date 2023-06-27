485cc9cd4ace0728aed8e466675f8257ce7abcd1
Ahmed <114447069+iAhmedddd@users.noreply.github.com>
2023-06-26 21:30:26 -0500

    Added data fetching

--- haven-for-homeless/src/pages/homePage.js
+++ haven-for-homeless/src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../Styles/homePage.css';
@@ -7,41 +7,49 @@ import Home_Image from '../assets/img/Homeland_Image.png';
 import Nav from '../navBar';
 import Footer from '../Footer';
 
-function Home(){
+function Home() {
+  const [data, setData] = useState(null);
 
-    return(
-        <div>   
+  // Fetching data
+  useEffect(() => {
+    fetch(process.env.PUBLIC_URL + '/package.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
 
-            {/* ======= Start Main ======= */}
-            <main>
+  if (!data) return <div>Loading...</div>;
 
-                <div className="header-blue">
-                    <Nav/>
-                    <div className="container hero">
-                        <div className="row">
-                        <div className="col-12 col-lg-6 col-xl-5 offset-xl-1 mt-5">
-                                <h1>Welcome to HavenForHomeless</h1>
-                                <p>We're dedicated to connecting those in need with local shelters and services.
-                                     Explore, find help, or contribute to your community today.
-                                </p>
-                                <Link to="/detail">
-                                    <button className="btn btn-light btn-lg action-button" type="button">Learn More</button>
-                                </Link>
-                            </div>
-                            <div className="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block Homeland_Image">
-                                <div className="Shelter"><img src={Home_Image} className="Homeland"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <Footer/>
+  return (
+    <div>
+      {/* ======= Start Main ======= */}
+      <main>
+        <div className="header-blue">
+          <Nav />
+          <div className="container hero">
+            <div className="row">
+              <div className="col-12 col-lg-6 col-xl-5 offset-xl-1 mt-5">
+                <h1>Welcome to HavenForHomeless</h1>
+                <p>We're dedicated to connecting those in need with local shelters and services.
+                  Explore, find help, or contribute to your community today.
+                </p>
+                <Link to="/detail">
+                  <button className="btn btn-light btn-lg action-button" type="button">Learn More</button>
+                </Link>
+              </div>
+              <div className="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block Homeland_Image">
+                <div className="Shelter"><img src={Home_Image} className="Homeland" />
                 </div>
-
-            </main>
-            {/* ======= End Main ======= */}
-          
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
-    );
+
+      </main>
+      {/* ======= End Main ======= */}
+    </div>
+  );
 }
 
 export default Home;
